ff2c325153ca350538e2d4de9def80f42ba54bea
peacethekuma <peacethekuma@gmail.com>
2019-12-02 21:42:25 +0800

    Added addItem & removeItem functionality to checkout item component

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,17 +1,48 @@
 import React from 'react';
 
 import './CheckoutItem.scss';
+import { connect } from 'react-redux';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from '../../redux/cart/actions';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, qunatity } }) => (
-  <div className='checkout-item'>
-    <div className='checkout-item__image-container'>
-      <img src={imageUrl} alt="item" />
+const CheckoutItem = ({ cartItem, clearItemFromCart, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='checkout-item__image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+      <span className='checkout-item__name'>{name}</span>
+      <span className='checkout-item__quantity'>
+        <div
+          className='checkout-item__arrow'
+          onClick={() => removeItem(cartItem)}
+        >
+          &#10094;
+        </div>
+        <span className='checkout-item__value'>{quantity}</span>
+        <div className='checkout-item__arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className='checkout-item__price'>{price}</span>
+      <div
+        className='checkout-item__remove-button'
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#10005;
+      </div>
     </div>
-    <span className='checkout-item__name'>{name}</span>
-    <span className='checkout-item__quantity'>{qunatity}</span>
-    <span className='checkout-item__price'>{price}</span>
-    <div className='checkout-item__remove-button'>&#10005;</div>
-  </div>
-)
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItemFromCart: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,5 +1,10 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './actions.js';
-import { addItemToCart } from './cart.utils';
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  REMOVE_ITEM,
+  CLEAR_ITEM_FROM_CART
+} from './actions.js';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,6 +22,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
       };
     default:
       return state;
