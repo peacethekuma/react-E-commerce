4018286398452ec45019490af8d4b8e09f89f96f
peacethekuma <peacethekuma@gmail.com>
2019-12-07 16:56:20 +0800

    Change case of filename

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
+
+const HomePage = props => {
+  console.log(props);
+  return (
+    <div className='homePage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
