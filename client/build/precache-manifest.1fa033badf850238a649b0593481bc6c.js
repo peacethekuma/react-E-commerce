c63289cf661370da6b832c166475254a70503ed3
peacethekuma <peacethekuma@gmail.com>
2020-02-08 20:35:15 +0800

    created server and modified project folder

---
+++ client/build/precache-manifest.1fa033badf850238a649b0593481bc6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06dc726a31bf8137597c4ca4bf813856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9ee04ad33b7a480c328",
+    "url": "/static/css/main.5cf8d5f1.chunk.css"
+  },
+  {
+    "revision": "09f755cc4c4ce1bca9a6",
+    "url": "/static/js/2.4105bf3d.chunk.js"
+  },
+  {
+    "revision": "e9ee04ad33b7a480c328",
+    "url": "/static/js/main.5db14f28.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
