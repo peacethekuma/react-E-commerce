3b0ec8a8cae7f72cdd6c8ef69f47659b0bd5f711
peacethekuma <peacethekuma@gmail.com>
2020-02-09 21:47:38 +0800

    Build server for stripe payment

--- client/src/components/StripeButton/StripeButton.js
+++ client/src/components/StripeButton/StripeButton.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_RNgXc83afeUV7R5BRDRF9fud00vHskT8I7';
-  const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
-  }
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful!');
+      })
+      .catch(error => {
+        console.log(`Payment error: ${JSON.parse(error)}`);
+        alert(
+          'There was an issue with your payment. Please sure you use the provided credit card.'
+        );
+      });
+  };
 
   return (
     <StripeCheckout
       label='Pay Now'
-      name="CRWN Clothing LTD."
+      name='CRWN Clothing LTD.'
       billingAddress
       shippingAddress
       image='https://sendeyo.com/up/d/f3eb2117da'
@@ -21,7 +38,7 @@ const StripeButton = ({ price }) => {
       token={onToken}
       stripeKey={publishableKey}
     />
-  )
-}
+  );
+};
 
 export default StripeButton;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -29,4 +31,19 @@ app.listen(port, error => {
     throw error;
   }
   console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    sourece: req.body.token.id,
+    amount: req.body.mount,
+    currency: 'usd'
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
